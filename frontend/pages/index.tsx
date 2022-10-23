a6ffe2307d4ec5eb5c636c8f18928f984821ae04
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-10-23 13:45:03 +0900

    add nft list

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import WalletConnectButton from '../components/atoms/WalletConnectButton'
 import Erc20TokenListModal from '../components/molecules/Erc20TokenListModal'
+import NFTListModal from '../components/molecules/NFTListModal'
 
 const Home: NextPage = () => {
   return (
     <div>
       <WalletConnectButton />
       <Erc20TokenListModal />
+      <NFTListModal />
     </div>
   )
 }
