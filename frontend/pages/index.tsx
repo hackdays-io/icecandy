5b1eb5c8625d4a2fde5872645a8bbdaec149d154
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-10-23 13:27:25 +0900

    listERC20 in polygon mumbai

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import WalletConnectButton from '../components/atoms/WalletConnectButton'
+import Erc20TokenListModal from '../components/molecules/Erc20TokenListModal'
 
 const Home: NextPage = () => {
   return (
     <div>
       <WalletConnectButton />
+      <Erc20TokenListModal />
     </div>
   )
 }
