62d559a0274a313e1d5fd1b5a43972ff75e1889d
MaySoMusician <maysomusician@gmail.com>
2022-10-26 13:33:51 +0900

    Enable Hardhat network only when appropriate env vars exist

--- protocol/hardhat.config.ts
+++ protocol/hardhat.config.ts
@@ -17,19 +17,27 @@ const LOCAL_PRIVATE_KEY = process.env.LOCAL_PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COIN_MARKET_CAP_API_KEY = process.env.COIN_MARKET_CAP_API_KEY
 
+type NetworkNameKey = 'mumbai' | 'local'
+const networks: { [network in NetworkNameKey]?: unknown } = {}
+
+if (ALCHEMY_API_KEY && ALCHEMY_API_KEY !== '' && PRIVATE_KEY && PRIVATE_KEY !== '') {
+  networks.mumbai = {
+    url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    accounts: [`${PRIVATE_KEY}`],
+  }
+}
+
+if (LOCAL_PRIVATE_KEY && LOCAL_PRIVATE_KEY !== '') {
+  networks.local = {
+    url: 'http://localhost:8545',
+    accounts: [`${LOCAL_PRIVATE_KEY}`],
+    allowUnlimitedContractSize: true,
+  }
+}
+
 module.exports = {
   solidity: '0.8.10',
-  networks: {
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`${PRIVATE_KEY}`],
-    },
-    local: {
-      url: 'http://localhost:8545',
-      accounts: [`${LOCAL_PRIVATE_KEY}`],
-      allowUnlimitedContractSize: true,
-    },
-  },
+  networks,
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
