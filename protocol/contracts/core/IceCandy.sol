599116eec1f5eacd5f655435666eac4198c50025
osaguild <33506754+osaguild@users.noreply.github.com>
2022-10-29 15:41:13 +0900

    refactor

--- protocol/contracts/core/IceCandy.sol
+++ protocol/contracts/core/IceCandy.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {IIceCandy} from "../interface/IIceCandy.sol";
+import {IIceCandy} from "../interfaces/IIceCandy.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract IceCandy is ERC721Enumerable, IIceCandy, Ownable {
--- protocol/contracts/core/Profile.sol
+++ protocol/contracts/core/Profile.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.10;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import {IProfile} from "../interface/IProfile.sol";
-import {INFTCollectionModule} from "../interface/INFTCollectionModule.sol";
+import {IProfile} from "../interfaces/IProfile.sol";
+import {INFTCollectionModule} from "../interfaces/INFTCollectionModule.sol";
 import {IceCandy} from "./IceCandy.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
 
 contract Profile is ERC721Enumerable, IProfile, Ownable {
     mapping(uint256 => IProfile.ProfileStruct) internal _profile;
