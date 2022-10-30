7f1a65e3328e69b83690699aa3c00f9b093c89a1
yu23ki14 <yuki_021423@yahoo.co.jp>
2022-10-30 23:52:55 +0900

    merge main

--- frontend/hooks/useENS.ts
+++ frontend/hooks/useENS.ts
@@ -2,22 +2,32 @@ import { useAddress } from '@thirdweb-dev/react'
 import { ethers } from 'ethers'
 import { useCallback, useEffect, useState } from 'react'
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 export const useENSName = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
   const [errors, setErrors] = useState<any>(null)
   const [ensName, setENSName] = useState<string | null>(null)
   const [ensPFP, setENSPFP] = useState<string | null>(null)
   const [ensDescription, setENSDescription] = useState<string | null>(null)
   const providerRpc = `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_ETH}`
-  const PROFILE_KEYS = ['url', 'description', 'com.twitter', 'email', 'com.github', 'notice', 'keywords', 'com.discord', 'com.reddit', 'org.telegram']
+  const PROFILE_KEYS = [
+    'url',
+    'description',
+    'com.twitter',
+    'email',
+    'com.github',
+    'notice',
+    'keywords',
+    'com.discord',
+    'com.reddit',
+    'org.telegram',
+  ]
 
   const address = useAddress()
 
   const getENSName = async () => {
-    setLoading(true)
     try {
-      const provider = new ethers.providers.JsonRpcProvider(providerRpc);
+      const provider = new ethers.providers.JsonRpcProvider(providerRpc)
       const _ensName = await provider.lookupAddress(String(address))
       console.log(_ensName)
       if (!_ensName) {
@@ -26,24 +36,25 @@ export const useENSName = () => {
       }
       const _ensPFP = await provider.getAvatar(String(address))
       const resolver = await provider.getResolver(_ensName!)
-      const results = await Promise.all(PROFILE_KEYS.map(async (key, index) => {
-        if (process.env.NEXT_PUBLIC_ENS_DELAY_TIME) {
-          await sleep(Number(process.env.NEXT_PUBLIC_ENS_DELAY_TIME) * index)
-        }
-        return resolver?.getText(key)
-      }))
-      const map = new Map<string, string | undefined>();
+      const results = await Promise.all(
+        PROFILE_KEYS.map(async (key, index) => {
+          if (process.env.NEXT_PUBLIC_ENS_DELAY_TIME) {
+            await sleep(Number(process.env.NEXT_PUBLIC_ENS_DELAY_TIME) * index)
+          }
+          return resolver?.getText(key)
+        })
+      )
+      const map = new Map<string, string | undefined>()
       results.forEach((res, index) => {
         map.set(PROFILE_KEYS[index], res)
       })
-      console.log(map)
 
       setENSName(_ensName)
       setENSPFP(_ensPFP)
       if (map.get('description')) setENSDescription(map.get('description')!)
     } catch (e: any) {
-      setErrors(e);
-      console.log(e);
+      setErrors(e)
+      console.log(e)
     } finally {
       setLoading(false)
     }
