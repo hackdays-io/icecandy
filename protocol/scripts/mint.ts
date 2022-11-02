fc98851801de112d950d0b106bfadf4efd7f8cf5
osaguild <nobuhisa.abe.0922@gmail.com>
2022-11-02 23:03:39 +0900

    add mint script

---
+++ protocol/scripts/mint.ts
@@ -0,0 +1,20 @@
+import { ethers } from 'hardhat'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { IceCandy__factory } from '../typechain-types'
+import * as dotenv from 'dotenv'
+const PATH_TO_HARDHAT_ENV = `${__dirname}/.env`
+dotenv.config({ path: PATH_TO_HARDHAT_ENV })
+
+async function main() {
+  const deployer = (await ethers.getSigners())[0] as SignerWithAddress
+  if (process.env.LOCAL_USER_ADDRESS && process.env.ICECANDY_ADDRESS) {
+    await IceCandy__factory.connect(process.env.ICECANDY_ADDRESS, deployer).mint(process.env.LOCAL_USER_ADDRESS)
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
