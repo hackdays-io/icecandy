ed3b8349c85707174fcc696f4d60efd8566744d4
osaguild <33506754+osaguild@users.noreply.github.com>
2022-11-03 17:58:49 +0900

    enable to specify wallet address

--- frontend/pages/test/mint-icecandy.tsx
+++ frontend/pages/test/mint-icecandy.tsx
@@ -1,13 +1,13 @@
-import { Box, Button } from '@chakra-ui/react'
-import { useAddress } from '@thirdweb-dev/react'
+import { useState } from 'react'
+import { Box, Button, Input } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import { useIceCandyContractClient } from '../../hooks/useContractClient'
 
 const TestIceCandyPage: NextPage = () => {
+  const [address, setAddress] = useState<string>()
   const iceCandyContract = useIceCandyContractClient({
     config: { requireWalletConnection: true },
   })
-  const address = useAddress()
 
   const mint = async () => {
     if (!iceCandyContract || !address) return
@@ -20,6 +20,11 @@ const TestIceCandyPage: NextPage = () => {
 
   return (
     <Box>
+      <Input
+        placeholder="address"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+      />
       <Button onClick={() => mint()}>mint</Button>
     </Box>
   )
